c922bae8b7b7f81ac5977e065cccb4251eff037a
Benjamin Simpson <benbangboom05@gmail.com>
2026-01-03 23:40:09 +0000

    3rd version

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 
+/// entry point of the application
 void main() {
+  // starts the app and attaches the widget tree
   runApp(const myApp());
 }
 
+/// root widget of the app
+/// responsible for app-wide setup
 class myApp extends StatelessWidget {
   const myApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      // first screen shown when the app starts
       home: homeScreen(),
     );
   }
 }
+ 
--- lib/widgets/empty_state.dart
+++ lib/widgets/empty_state.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+
+/// widget shown when there are no tasks
+class emptyState extends StatelessWidget {
+  const emptyState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      // card gives a clear visual container
+      child: Padding(
+        // spacing inside the card
+        padding: EdgeInsets.all(16),
+
+        // message shown when the task list is empty
+        child: Text(
+          'no tasks yet',
+        ),
+      ),
+    );
+  }
+}
 
